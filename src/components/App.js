72242111838e88ec231585556290dcf9d6476fe6
ohtaekwon <ohtk1119@gmail.com>
2022-05-16 21:33:06 +0900

    ReviewList

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 // 프로젝트의 최상위 컴포넌트
 import ReviewList from "./ReviewList";
-import items from "../mock.json";
+import mockItems from "../mock.json";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(mockItems);
+  const [order, setOrder] = useState("createdAt");
+  const sortedItems = items.sort((a, b) => b[order] - a[order]);
+  // 최신순
+  const handleNewstClick = () => {
+    setOrder("createdAt");
+  };
+  // 평점순
+  const handleBestClick = () => {
+    setOrder("rating");
+  };
+  // 삭제
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
   return (
     <div>
-      <ReviewList items={items} />
+      <div>
+        <button onClick={handleNewstClick}>최신순</button>
+        <button onClick={handleBestClick}>평점순</button>
+      </div>
+      <ReviewList items={sortedItems} onDelete={handleDelete} />
     </div>
   );
 }
--- src/components/ReviewList.js
+++ src/components/ReviewList.js
@@ -1,6 +1,40 @@
-function ReviewList({ items }) {
-  console.log(items);
-  return <ul></ul>;
+import "../css/ReviewList.css";
+
+function formatData(value) {
+  const data = new Date(value);
+  return `${data.getFullYear()}. ${data.getMonth() + 1}. ${data.getDate()}`;
+}
+
+function ReviewListItem({ item, onDelete }) {
+  const handleDeleteClick = () => onDelete(item.id);
+
+  return (
+    <div className="ReviewListItem">
+      <img className="ReviewListItem-img" src={item.imgUrl} alt={item.title} />
+      <div>
+        <h1>{item.title}</h1>
+        <p>{item.rating}</p>
+        <p>{formatData(item.createdAt)}</p>
+        <p>{item.content}</p>
+        <button onClick={handleDeleteClick}>삭제하기</button>
+      </div>
+    </div>
+  );
+}
+
+function ReviewList({ items, onDelete }) {
+  return (
+    <ul>
+      {items.map((item) => {
+        return (
+          <li key={item.id}>
+            <ReviewListItem item={item} onDelete={onDelete} />
+            <input></input>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default ReviewList;
