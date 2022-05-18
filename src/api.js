157849279a1d0b9730b2d9736668cb8bc26229d8
ohtaekwon <ohtk1119@gmail.com>
2022-05-18 18:02:19 +0900

    offset

--- src/api.js
+++ src/api.js
@@ -1,5 +1,9 @@
-export async function getRewviews(order = "createdAt") {
-  const query = `order=${order}`;
+export async function getRewviews({
+  order = "createdAt",
+  offset = 0,
+  limit = 6,
+}) {
+  const query = `order=${order}&offset=${offset}&limit=${limit}`;
   const res = await fetch(`https://learn.codeit.kr/api/film-reviews?${query}`);
   console.log(res);
   const body = await res.json(); // json 함수를 호출
--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import ReviewList from "./ReviewList";
 import { useEffect, useState } from "react";
 import { getRewviews } from "../api";
 
+const Limit = 6;
+
 function App() {
   const [items, setItems] = useState([]);
   const [order, setOrder] = useState("createdAt");
+  // offset state
+  const [offset, setOffset] = useState(0);
+  // hasNext
+  const [hasNext, setHasNext] = useState(false);
   const sortedItems = items.sort((a, b) => b[order] - a[order]);
   // 최신순
   const handleNewstClick = () => {
@@ -21,14 +27,26 @@ function App() {
     const nextItems = items.filter((item) => item.id !== id);
     setItems(nextItems);
   };
+  // 처음 출력
+  const handleLoad = async (options) => {
+    const { reviews, paging } = await getRewviews(options);
+    if (options.offset === 0) {
+      setItems(reviews);
+    } else {
+      setItems([...items, ...reviews]);
+    }
+    setOffset(options.offset + reviews.length); // 6
+    setHasNext(paging.hasNext);
+  };
 
-  const handleLoad = async (orderQuery) => {
-    const { reviews } = await getRewviews(orderQuery);
-    setItems(reviews);
+  // 더 불러오기
+  const handLoadMore = () => {
+    // 다음 페이지
+    handleLoad({ order, offset, limit: Limit });
   };
 
   useEffect(() => {
-    handleLoad(order);
+    handleLoad({ order, offset: 0, limit: Limit });
   }, [order]);
 
   return (
@@ -39,6 +57,7 @@ function App() {
       </div>
       <ReviewList items={sortedItems} onDelete={handleDelete} />
       {/* <button onClick={handleLoadClick}>불러오기</button> */}
+      {hasNext && <button onClick={handLoadMore}>더 보기</button>}
     </div>
   );
 }
